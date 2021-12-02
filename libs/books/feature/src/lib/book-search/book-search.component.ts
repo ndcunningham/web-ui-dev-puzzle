f9dd8ee00f969f4ef9ca121c56d3762a3f738401
Nicholas Cunningham <ndcunningham@gmail.com>
2021-12-02 13:03:26 -0700

    (chore) - fix broken test cases and add code review
    Remove unsubscribed subscription in favour of async pipe inside of the template
    Removed formatDate function which was called only on the template in favour of angular date pipe.
    Add unimplemented functionality for unit tests
    Add missing accessibility

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -4,19 +4,19 @@ import {
   addToReadingList,
   clearSearch,
   getAllBooks,
-  ReadingListBook,
   searchBooks
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'tmo-book-search',
   templateUrl: './book-search.component.html',
   styleUrls: ['./book-search.component.scss']
 })
-export class BookSearchComponent implements OnInit {
-  books: ReadingListBook[];
+export class BookSearchComponent {
+  books$ = this.store.select(getAllBooks);
 
   searchForm = this.fb.group({
     term: ''
@@ -25,22 +25,10 @@ export class BookSearchComponent implements OnInit {
   constructor(
     private readonly store: Store,
     private readonly fb: FormBuilder
-  ) {}
+  ) { }
 
   get searchTerm(): string {
     return this.searchForm.value.term;
-  }
-
-  ngOnInit(): void {
-    this.store.select(getAllBooks).subscribe(books => {
-      this.books = books;
-    });
-  }
-
-  formatDate(date: void | string) {
-    return date
-      ? new Intl.DateTimeFormat('en-US').format(new Date(date))
-      : undefined;
   }
 
   addBookToReadingList(book: Book) {
