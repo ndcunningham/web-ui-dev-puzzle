617f0e3dcd6544fd6086f2bce13cba96d2830bc2
Nicholas Cunningham <ndcunningham@gmail.com>
2021-12-03 13:00:56 -0700

    (feat) - Add undo actions and show notifications
    When you add/remove a book from your reading list
    There will be a snackbar to shows up.
    The snack bar also gives the opportunity to undo the previous action.
    Adding Tests for these scenarios

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -11,4 +11,32 @@ describe('When: I use the reading list feature', () => {
       'My Reading List'
     );
   });
+
+  describe(' and click on Want to Read', () => {
+    it('Then: I click Want to Read should show Notification', () => {
+      cy.get('input[type="search"]').type('javascript');
+      cy.get('form').submit();
+
+      cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 0);
+
+      cy.get('[data-testing="want-to-read"]').first().click();
+
+      cy.get('snack-bar-container').should('exist');
+      cy.get('[data-testing="reading-list-item"]').should('have.length', 1);
+    });
+
+    it('Then: Undo should restore after adding a Book', () => {
+      cy.get('input[type="search"]').type('javascript');
+      cy.get('form').submit();
+
+      cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 0);
+
+      cy.get('[data-testing="want-to-read"]').first().click();
+      cy.get('snack-bar-container').should('exist');
+      cy.get('snack-bar-container button').should('contain.text', 'Undo')
+
+      cy.get('snack-bar-container button').click();
+      cy.get('[data-testing="reading-list-item"]').should('have.length', 0);
+    });
+  });
 });
